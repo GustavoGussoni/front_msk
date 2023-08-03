7108510db35af8e93630938467ff15ea1eeb9c75
Gustavo Gussoni <gussonigq@gmail.com>
2023-08-03 20:57:29 -0300

    feat: create player

--- src/schemas/music.schema.ts
+++ src/schemas/music.schema.ts
@@ -8,7 +8,19 @@ export const musicSchema = z.object({
   genre: z.string(),
   year: z.string(),
   cover_image: z.string(),
-  music_url: z.string(),
+  music_url: z.string()
 });
 
 export type musicData = z.infer<typeof musicSchema>;
+
+export interface CurrentMusicType extends musicData {
+  duration?: number;
+  curTime?: number;
+  isPlaying?: boolean;
+}
+
+export type PlayerContextType = {
+  currentMusic: CurrentMusicType;
+  setCurrentMusic: (cm: Partial<CurrentMusicType>, replace?: boolean) => void;
+  playList: musicData[];
+};
