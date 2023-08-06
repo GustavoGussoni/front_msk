43af73f4169810a54d7beda5ccc7876756791dbe
Gustavo Gussoni <gussonigq@gmail.com>
2023-08-06 11:45:56 -0300

    feat: create login register

---
+++ src/contexts/authContext.tsx
@@ -0,0 +1,55 @@
+import { LoginData, UserData } from "@/schemas/user.schema";
+import api from "@/services/api";
+import { useRouter } from "next/router";
+import { ReactNode, createContext } from "react";
+import { toast } from "react-toastify";
+
+interface Props {
+  children: ReactNode;
+}
+
+interface authProviderData {
+  setToken: (value: string) => void;
+  register: (userData: UserData) => void;
+  login: (loginData: LoginData) => void;
+  token: string | undefined;
+}
+
+const AuthContext = createContext<authProviderData>({} as authProviderData);
+
+export const AuthProvider = ({ children }: Props) => {
+  const router = useRouter();
+
+  const register = (userData: UserData) => {
+    api
+      .post("/users", userData)
+      .then(() => {
+        toast.success("Usuário cadastrado com sucesso!", {
+          position: "top-right",
+          autoClose: 2000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          draggable: true,
+          pauseOnHover: true,
+          progress: undefined,
+          theme: "light"
+        });
+        router.push("/login");
+      })
+      .catch((err) => {
+        console.log(err);
+        toast.error("Erro ao criar usuário, tente um email diferente.", {
+          position: "top-right",
+          autoClose: 2000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          draggable: true,
+          pauseOnHover: true,
+          progress: undefined,
+          theme: "light"
+        });
+      });
+  };
+
+  const login = () => {};
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,26 @@
 import { PlayerProvider } from "@/contexts/playerContext";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ToastContainer } from "react-toastify";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <PlayerProvider>
-      <Component {...pageProps} />
-    </PlayerProvider>
+    <>
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      <PlayerProvider>
+        <Component {...pageProps} />
+      </PlayerProvider>
+    </>
   );
 }
